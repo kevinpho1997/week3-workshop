a5fa7ace26dc5585171c127e44073142b865fd4b
kevinpho1997 <kevin.pho@griffithuni.edu.au>
2022-08-05 01:35:42 +1000

    added home routing added account page

---
+++ routes/homeroute.js
@@ -0,0 +1,10 @@
+module.exports = {
+    route: (app, path) => {
+        // app - passes in the express object needed for the route.
+        // path - passes in a path object needed to find the file. The path module is part of node and needs to be required in the server.js file
+        app.get('/', function(req, res){
+            let filepath = path.resolve('../www/index.html');
+            res.sendFile(filepath);
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+const { path } = require('express/lib/application');
 const { hostname } = require('os');
 var app = express();
 var http = require('http').Server(app);
 app.use(express.static(__dirname + '/www'));
+
+// home route
+require('/routes/homeroute.js').route(app, path);
 
 let server = http.listen(3000, function () {
     let host = server.address().address;
@@ -17,4 +21,14 @@ app.get('/test', function(req, res){
 
 app.get('/login', function(req, res){
     res.sendFile(__dirname + '/www/login.html');
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send(`Username: ${username} Password: ${password}`);
+});
+
+app.get('/account', function(req, res){
+    res.sendFile(__dirname + '/www/account.html');
 });
